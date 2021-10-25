b14a17bbaf2a3672dca1349d9600d5fdf669a720
guoxiaoyang395 <guoxiaoyang395@hellobike.com>
2021-10-25 20:07:10 +0800

    feat(): complete md

--- src/index.js
+++ src/index.js
@@ -39,9 +39,3 @@ patch(el, patches)
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals()
-
-// 梳理dom-diff的总过程
-// 1.使用js对象模拟虚拟dom（虚拟dom）
-// 2. 将虚拟dom转为真实dom，插入到某个节点下（render）
-// 3. 对比新旧虚拟dom的差异，将差异保存在patches中（diff）
-// 4. 将差异对象应用到真实dom中（patch）
